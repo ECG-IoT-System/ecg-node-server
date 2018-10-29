fdd7d7f5a055b2f7ebbfe363696645cb4ef8d51b
pei <pei@peide-MacBook-Pro.local>
2018-10-29 17:24:34 +0800

    update

--- connect_sql.js
+++ connect_sql.js
@@ -1,31 +1,31 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host: '',
+  host: process.env.MYSQL_URL || "localhost",
   user: 'root',
   password: 'root',
   database: 'new_schema'
 });
 
+connection.connect(function(err) {
+  if (err) {
+    return console.error('1.error: ' + err.message);
+  }
+  console.log('2.Connected to the MySQL server.');
+});
 
 function insert(value) {
   console.log("insert");
-  var sql = 'INSERT INTO ECG3LTIME_Advantech(deviceid,time,data) VALUES(?,?,?)';
-  var addSqlParams = [];
+  var sql = `INSERT INTO ECG3LTIME_Advantech(deviceID,time,data) VALUES ?`;
+  var addSqlParams =[];
   var count = value[0].count;
+  console.log(count)
   for (var i = 1; i <= count; i++)
     addSqlParams.push([value[i].deviceid, value[i].time, value[i].data]);
-  console.log(addSqlParams);
-  connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-   
-    console.log('Connected to the MySQL server.');
-  });
+  console.log('addsql: ',addSqlParams);
   
   connection.query(sql, [addSqlParams], function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
+    if (error)
+    console.log('3.The solution is: ',error.message);
   });
 
 }
